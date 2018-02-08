fix scaling issue with training data for word classifier, add matplotlib scatter to visualize dataset for words

wine-board.py
@@ -299,15 +299,14 @@ class WineBoard(object):
             for idx in range(0, len(wine)):
                 if wine[idx] == 1:
                     resultMatrix[i][1].add(self.categories[idx])
-        #for i in range(0, len(resultMatrix)):
-            #if i < len(self.y_wine_pred):
-                #print "Expected: {} - Result: {}".format(resultMatrix[i][0], list(resultMatrix[i][1]))
-            #else:
-                #print "Result: {}".format(list(resultMatrix[i][1]))
-                #print ' '.join(words[i])
+#        for i in range(0, len(resultMatrix)):
+#            if i < len(self.y_wine_pred):
+#                print "Expected: {} - Result: {}".format(resultMatrix[i][0], list(resultMatrix[i][1]))
+#            else:
+#                print "Result: {}".format(list(resultMatrix[i][1]))
+#                print ' '.join(words[i])
 
-            #print "Review: \"{}\"".format(i[2])
-        #self.WineClassifier.show()
+#            print "Review: \"{}\"".format(i[2])
         transformed_raw = []
         raw_words = np.array(raw_words)
         for i in raw_words:
@@ -315,11 +314,14 @@ class WineBoard(object):
                 transformed_raw.append(j)
 
         raw_words = np.concatenate((self.x_train, np.array(transformed_raw)), axis=0)
-        print raw_words[:, 0]
-        print raw_words[:, 1]
-        #plt.scatter(self.x_train[:, 0], self.x_train[:, 1], alpha=0.5)
-        plt.scatter(raw_words[:, 0], raw_words[:, 1], c='r', alpha=0.5)
-        plt.show()
+        def onpick(event):
+            ind = event.ind
+            print event
+            print ind
+        fig, ax = plt.subplots()
+        col = ax.scatter(raw_words[:, 0], raw_words[:, 1], c='r', alpha=0.5, picker=True)
+        fig.canvas.mpl_connect('pick_event', onpick)
+        print "Please show plot"
 
     def parse_reviews(self, n):
         reviews = []
@@ -397,3 +399,4 @@ print categories
 myBoard = WineBoard(stoplist, categories, keywords, weights)
 myBoard.train_words(4)
 myBoard.train_wines(220)
+plt.show()